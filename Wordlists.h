d80397d0031fc88996930287314e24d198120dc4
BGregory577 <113824546+BGregory577@users.noreply.github.com>
2022-09-26 20:33:48 -0600

    Create Wordlists.h
    Created a word file for the wordlist array

---
+++ Wordlists.h
@@ -0,0 +1,4 @@
+#pragma once
+
+const char* wordlist[49]		//Wordlist is a little bit bigger
+= { "hello", "world", "name", "coding", "beautiful", "ability", "administration", "agency", "american", "audience", "behavior", "business", "candidate", "challenge", "collection", "computer", "daughter", "democratic", "discussion", "economy", "environment", "evidence", "federal", "financial", "friend", "generation", "government", "hospital", "information", "international", "knowledge", "machine", "management", "message", "movement", "necessary", "organization", "participate", "population", "professional", "recognize", "responsibility", "security", "significant", "statement", "technology", "traditional", "violence", "western"};		//word list in an array
